fd16053279af4229ba12e356124a75893f8374e7
Günseli Ünsal <gunseliunsal@hotmail.com>
2023-07-29 18:19:40 +0300

    added bottom navbar

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,22 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+
+  void Function(int)? onTabChange;
+
+  MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(tabs: [
-        GButton(
-          icon: Icons.home,
-          text: "Shop",
-        ),
-        GButton(
-          icon: Icons.shopping_bag_rounded,
-          text: "Card",
-        ),
-
-      ]),
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: GNav(
+          color: Colors.grey[400],
+          activeColor: Colors.grey.shade700,
+          tabActiveBorder: Border.all(color: Colors.white),
+          tabBackgroundColor: Colors.grey.shade100,
+          mainAxisAlignment: MainAxisAlignment.center,
+          tabBorderRadius: 16,
+          onTabChange: (value) => onTabChange!(value),
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              text: "Shop",
+            ),
+            GButton(
+              icon: Icons.shopping_bag_rounded,
+              text: "Card",
+            ),
+          ]),
     );
   }
 }
